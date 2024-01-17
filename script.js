93eaafef74d019f8d72e05f0e6d79d6837684f64
DantNciso <121272402+DantNciso@users.noreply.github.com>
2024-01-17 10:11:49 -0600

    Update script.js

--- script.js
+++ script.js
@@ -20,7 +20,7 @@ c.height = window.innerHeight;
 c.width = window.innerWidth;
 
 // the characters
-var konkani  = "Buenos Dias!"
+var konkani  = "Caja Popular La Providencia 2024"
 // converting the string into an array of single characters
 var characters = konkani.split("");
 var font_size = 14;
